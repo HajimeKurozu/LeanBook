1c1eeafc0c8462146a66f9aaf8c8fa88571870e9
Hajime Kurozu <hajimek68@gmail.com>
2026-02-28 08:17:58 +0900

    第3章3節の追加

---
+++ TPIL/Chapter3/Section3.lean
@@ -0,0 +1,17 @@
+/-
+
+3.3.命題論理
+
+Leanは、すべての標準的な論理接続と表記を定義します。命題接続詞は以下の表記とともに来ます。
+
+
+ASCII Unicode Editor shortcut Definition
+True                          True
+False                         False
+Not   ¬       \not, \neg.     Not
+/\    ∧.      \and.           And
+\/.   ∨.      \or.            Or
+->.   →.      \to, \r, \imp.
+<->.  ↔.      \iff, \lr.      Iff
+
+-/
