41d4511b5426c16b364a86c2f7bfd9618493932c
Hajime Kurozu <hajimek68@gmail.com>
2026-03-04 06:48:54 +0900

    コードの追加完了(3.2)

--- mathematics_in_lean/MIL/C03_Logic/S02_The_Existential_Quantifier.lean
+++ mathematics_in_lean/MIL/C03_Logic/S02_The_Existential_Quantifier.lean
@@ -152,7 +152,9 @@ example {c : ℝ} : Surjective fun x ↦ x + c := by
   dsimp; ring
 
 example {c : ℝ} (h : c ≠ 0) : Surjective fun x ↦ c * x := by
-  sorry
+  intro x
+  use x / c
+  field_simp
 
 example (x y : ℝ) (h : x - y ≠ 0) : (x ^ 2 - y ^ 2) / (x - y) = x + y := by
   field_simp [h]
@@ -172,6 +174,11 @@ variable {α : Type*} {β : Type*} {γ : Type*}
 variable {g : β → γ} {f : α → β}
 
 example (surjg : Surjective g) (surjf : Surjective f) : Surjective fun x ↦ g (f x) := by
-  sorry
+  intro z
+  rcases surjg z
+  with ⟨y, rfl⟩
+  rcases surjf y
+  with ⟨x, rfl⟩
+  use x
 
 end
