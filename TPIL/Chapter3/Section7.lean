8b2e553c79b14464295f49ad92dd8c7e682d780d
Hajime Kurozu <hajimek68@gmail.com>
2026-03-05 05:31:56 +0900

    第3章7節コードの追加

--- TPIL/Chapter3/Section7.lean
+++ TPIL/Chapter3/Section7.lean
@@ -157,3 +157,144 @@ example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
             (fun hr : r =>
               -- q と r が揃ったので、結論の右側 (q ∧ r) を作ります
               show p ∨ (q ∧ r) from Or.inr ⟨hq, hr⟩)))
+
+example : (p → (q → r)) ↔ (p ∧ q → r) :=
+  Iff.intro
+    (fun h : p → (q → r) =>
+      -- 左から右への証明 (→)
+      -- 引数として (p ∧ q) を受け取る関数を定義します
+      fun hpq : p ∧ q =>
+        -- p ∧ q を分解してそれぞれのパーツを取り出す
+        have hp : p := hpq.left
+        have hq :q := hpq.right
+        -- h に hp を渡すと (q → r) が返り、そこに hq を渡すと r が得られる
+        show r from h hp hq)
+    (fun h : p ∧ q → r =>
+      -- 右から左への証明 (←)
+      -- p を受け取り、「q を受け取って r を返す関数」を返す
+      fun hp : p =>
+        fun hq : q =>
+        -- p と q を組み合わせて (p ∧ q) を作る
+        have hpq : p ∧ q := ⟨hp, hq⟩
+        -- h に hpq を渡して r を得る
+        show r from h hpq)
+
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+  Iff.intro
+    (fun h : (p ∨ q) → r =>
+      -- 左から右への証明 (→)
+      -- 1. (p → r) を作る
+      have hpr : p → r :=
+        fun hp : p =>
+          show r from h (Or.inl hp)
+      -- 2. (q → r) を作る
+      have hqr : q → r :=
+        fun hq : q =>
+          show r from h (Or.inr hq)
+      -- 3. 両方を組み合わせて And (∧) を作る
+      show (p → r) ∧ (q → r) from ⟨hpr, hqr⟩)
+    (fun h : (p → r) ∧ (q → r) =>
+      -- 右から左への証明 (←)
+      -- 引数として (p ∨ q) を受け取り、r を返す関数を作る
+      fun hpq : p ∨ q =>
+        -- ∧ を分解してそれぞれの関数を取り出す
+        have hpr : p → r := h.left
+        have hqr : q → r := h.right
+        -- (p ∨ q) を場合分けして、適切な関数を適用する
+        Or.elim hpq
+          (fun hp : p => show r from hpr hp)
+          (fun hq : q => show r from hqr hq))
+
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+  Iff.intro
+    (fun h : ¬(p ∨ q) =>
+      -- 左から右への証明 (→)
+      -- 1. ¬p (p → False) を導く
+      have hnp : ¬p :=
+      fun hp : p =>
+        show False from h (Or.inl hp)
+      -- 2. ¬q (q → False) を導く
+      have hnq : ¬q :=
+        fun hq : q =>
+          show False from h (Or.inr hq)
+      -- 3. それらを組み合わせて ∧ を作る
+      show ¬p ∧ ¬q from ⟨hnp, hnq⟩)
+    (fun h : ¬p ∧ ¬q =>
+      -- 右から左への証明 (←)
+      -- 1. (p ∨ q) を仮定して False を導く関数を作る
+      fun hpq : p ∨ q =>
+        have hnp : ¬p := h.left
+        have hnq : ¬q := h.right
+        -- 2. (p ∨ q) を場合分けして、それぞれ矛盾(False)を出す
+        Or.elim hpq
+          (fun hp : p => show False from hnp hp)
+          (fun hq : q => show False from hnq hq))
+
+example (p q : Prop) : ¬p ∨ ¬q → ¬(p ∧ q) :=
+  -- 1. 仮定 h : ¬p ∨ ¬q を受け取り、結論 ¬(p ∧ q) を導く関数を作る
+  fun h : ¬p ∨ ¬q =>
+    -- 2. ¬(p ∧ q) つまり (p ∧ q) → False を証明するため、引数 hpq を取る
+    fun hpq : p ∧ q =>
+      -- 3. hpq を分解して p と q を手に入れる
+      have hp : p := hpq.left
+      have hq : q := hpq.right
+      -- 4. 最初の仮定 h (¬p ∨ ¬q) を場合分けする
+      show False from Or.elim h
+        (fun hnp : ¬p =>
+          -- ¬p (p → False) に hp を渡して矛盾を出す
+          show False from hnp hp)
+        (fun hnq : ¬q =>
+          -- ¬q (q → False) に hq を渡して矛盾を出す
+          show False from hnq hq)
+
+example (p : Prop) : ¬(p ∧ ¬p) :=
+  -- 1. ¬(p ∧ ¬p) は (p ∧ ¬p) → False と同義。
+  -- そのため、(p ∧ ¬p) を仮定として受け取る関数を書く。
+  fun h : p ∧ ¬p =>
+    -- 2. 仮定 h から、p と ¬p をそれぞれ取り出す。
+    have hp  : p  := h.left
+    have hnp : ¬p := h.right
+    -- 3. ¬p は (p → False) なので、これに hp を渡せば False が得られる。
+    show False from hnp hp
+
+example (p q : Prop) : p ∧ ¬q → ¬(p → q) :=
+  -- 1. 仮定 h : p ∧ ¬q を受け取る
+  fun h : p ∧ ¬q =>
+    -- 2. ¬(p → q) を証明するため、(p → q) を仮定して False を導く
+    fun hpq : p → q =>
+      -- 3. 最初の大切な仮定 h をバラす
+      have hp  : p  := h.left
+      have hnq : ¬q := h.right -- hnq は (q → False) と同義
+      -- 4. hpq に hp を渡すと q が得られる
+      have hq : q := hpq hp
+      -- 5. 得られた q を hnq に渡して矛盾(False)を導く
+      show False from hnq hq
+
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ False ↔ p := sorry
+example : p ∧ False ↔ False := sorry
+example : (p → q) → (¬q → ¬p) := sorry
+
+/-
+以下の識別子を証明し、sorryプレースホルダーを実際の証明に置き換えてください。
+これらは古典的な推論を必要とします。
+-/
+
+open Classical
+
+variable (p q r : Prop)
+
+example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := sorry
+example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
+example : ¬(p → q) → p ∧ ¬q := sorry
+example : (p → q) → (¬p ∨ q) := sorry
+example : (¬q → ¬p) → (p → q) := sorry
+example : p ∨ ¬p := sorry
+example : (((p → q) → p) → p) := sorry
+
+/-
+古典論理を使用せずに ¬(p ↔ ¬p) を証明してください。
+-/
+
+example : ¬(p ↔ ¬p) := sorry
